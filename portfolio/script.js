2dd8fc1b9168ac0612202d86bdded9cec1a1ef71
Archit Sisodia (EXT-Nokia) <archit.sisodia.ext@nokia.com>
2026-03-11 12:08:00 +0530

    updated the style of portfolio

--- portfolio/script.js
+++ portfolio/script.js
@@ -1,3 +1,19 @@
-document.addEventListener("DOMContentLoaded", function() {
-    console.log("Portfolio Loaded Successfully 🚀");
-});
+const text = "DevOps Engineer | AWS | Kubernetes | Terraform";
+
+let i = 0;
+
+function typing(){
+
+if(i < text.length){
+
+document.getElementById("typing").innerHTML += text.charAt(i);
+
+i++;
+
+setTimeout(typing,70);
+
+}
+
+}
+
+typing();
